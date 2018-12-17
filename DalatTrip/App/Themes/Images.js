054c01e2a3ea7c73fa7700f163640e669fab4745
LAPTOP <thinhbocrazy@yahoo.com>
2018-12-17 20:54:09 +0700

    Update Theme/images for linking image-source

--- DalatTrip/App/Themes/Images.js
+++ DalatTrip/App/Themes/Images.js
@@ -1,4 +1,3 @@
-// leave off @2x/@3x
 const images = {
   launch: require('../Images/launch.jpg'),
   homeBg: require('../Images/homebg.jpg'),
@@ -24,56 +23,56 @@ const images = {
 const category = {
   place: {
     main: require('../Images/places.png'),
-    anBang: require('../Images/places/AnBang.jpg'),
-    fujian: require('../Images/places/fujian-assembly-hall.jpg'),
-    japaneseBridge: require('../Images/places/japanese_bridge.jpg'),
-    ceramics: require('../Images/places/Hoi-An-Museum-of-Trade-Ceramics.jpg'),
-    lune: require('../Images/places/LunePerformingCenter.jpg'),
-    history: require('../Images/places/Museum-of-History-Culture.jpg'),
-    heritage: require('../Images/places/precious-heritage.jpg'),
-    oldHouse: require('../Images/places/the-old-house-of-phun.jpg'),
-    traQue: require('../Images/places/Tra-Que-Vegetable-Village.jpg'),
-    vanDuc: require('../Images/places/van-duc-pagoda.jpg')
+    CuLan: require('../Images/places/CuLan.jpg'),
+    Elephant: require('../Images/places/Elephant.jpg'),
+    LamVien: require('../Images/places/LamVien.jpg'),
+    LangBiang: require('../Images/places/LangBiang.jpg'),
+    Lights: require('../Images/places/Lights.jpg'),
+    Love: require('../Images/places/Love.jpg'),
+    LPhuoc: require('../Images/places/LPhuoc.jpg'),
+    Park: require('../Images/places/Park.jpg'),
+    Station: require('../Images/places/Station.jpg'),
+    TuyenLam: require('../Images/places/TuyenLam.jpg')
   },
   hotel: {
     main: require('../Images/hotel_screen.png'),
-    almond: require('../Images/hotel/almond.jpg'),
-    anantara: require('../Images/hotel/anantara.jpg'),
-    ancient: require('../Images/hotel/ancient_house.jpg'),
-    bamboo: require('../Images/hotel/bamboo.jpg'),
-    gialam: require('../Images/hotel/gialam.jpg'),
-    hippie: require('../Images/hotel/hippie_house.jpg'),
-    historic: require('../Images/hotel/historic.jpg'),
-    silk: require('../Images/hotel/silk_marina.jpg'),
-    victoria: require('../Images/hotel/victoria.jpg'),
-    vinhhung: require('../Images/hotel/vinhhung.jpg')
+    Backpacker: require('../Images/hotel/Backpacker.jpg'),
+    HangNga: require('../Images/hotel/HangNga.jpg'),
+    Marguerite: require('../Images/hotel/Marguerite.jpg'),
+    Monet: require('../Images/hotel/Monet.jpg'),
+    NgocPhat: require('../Images/hotel/NgocPhat.jpg'),
+    Sammy: require('../Images/hotel/Sammy.jpg'),
+    Sapinette: require('../Images/hotel/Sapinette.jpg'),
+    Sky: require('../Images/hotel/Sky.jpg'),
+    Tiger: require('../Images/hotel/Tiger.jpg'),
+    ZenValley: require('../Images/hotel/ZenValley.jpg')
   },
   food: {
     main: require('../Images/food.png'),
-    aubergine49: require('../Images/food/aubergine49-restaurant.jpg'),
-    frenchBakery: require('../Images/food/french-bakery-and-Restaurant.jpg'),
-    herbs: require('../Images/food/herbs-and-spices-restaurant.jpg'),
-    nhans: require('../Images/food/nhans-kitchen.jpg'),
-    cafe: require('../Images/food/restaurant-cafe-tu-n.jpg'),
-    rosie: require('../Images/food/rosies-cafe.jpg'),
-    shore: require('../Images/food/shore-club-an-bang-beach.jpg'),
-    simple: require('../Images/food/simple-restaurant-and.jpg'),
-    temple: require('../Images/food/temple-restaurant.jpg'),
-    traQue: require('../Images/food/tra-que-garden.jpg')
+    Biang: require('../Images/food/Biang.jpg'),
+    Bistro: require('../Images/food/Bistro.jpg'),
+    Daquy: require('../Images/food/Daquy.jpg'),
+    FrenchTouch: require('../Images/food/FrenchTouch.jpg'),
+    Hanh: require('../Images/food/Hanh.jpg'),
+    Petit: require('../Images/food/Petit.jpg'),
+    Quan: require('../Images/food/Quan.jpg'),
+    Rabelais: require('../Images/food/Rabelais.jpg'),
+    Wheel: require('../Images/food/Wheel.jpg'),
+    Alley: require('../Images/food/Alley.jpg'),
   },
 
   shop: {
     main: require('../Images/shops.png'),
-    friendly_shop: require('../Images/shop/friendly_shop.jpg'),
-    de_lantana: require('../Images/shop/de_lantana.jpg'),
-    night_market: require('../Images/shop/night_market.jpg'),
-    central_market: require('../Images/shop/central_market.jpg'),
-    hoian_lantern_shop: require('../Images/shop/hoian_lantern_shop.jpg'),
-    coleurs_dasie: require('../Images/shop/coleurs_dasie.jpg'),
-    hunglong_gallery: require('../Images/shop/hunglong_gallery.jpg'),
-    reaching_out: require('../Images/shop/reaching_out.jpg'),
-    sunny_shop: require('../Images/shop/sunny_shop.jpg'),
-    a_dong_silk: require('../Images/shop/a_dong_silk.jpg')
+    DLnature: require('../Images/shop/DLnature.jpg'),
+    Labata: require('../Images/shop/Labata.jpg'),
+    Lana: require('../Images/shop/Lana.jpg'),
+    Langfarm: require('../Images/shop/Langfarm.jpg'),
+    Lavender: require('../Images/shop/Lavender.jpg'),
+    Market: require('../Images/shop/Market.jpg'),
+    Namsilk: require('../Images/shop/Namsilk.jpg'),
+    TG: require('../Images/shop/TG.jpg'),
+    Trickland: require('../Images/shop/Trickland.jpg'),
+    XQ: require('../Images/shop/XQ.jpg')
   }
 }
 
