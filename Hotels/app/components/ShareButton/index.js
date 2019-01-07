6f0ffd8b6198a938fd1a0f4e3afe0bbf659910ff
LAPTOP <thinhbocrazy@yahoo.com>
2019-01-07 20:51:01 +0700

    final stable

--- Hotels/app/components/ShareButton/index.js
+++ Hotels/app/components/ShareButton/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import Icon from 'react-native-vector-icons/Feather';
-import { Share } from 'react-native';
+import { Share, TouchableOpacity, Image } from 'react-native';
 
 export default class ShareButton extends Component {
   render() {
     return (
-      <Icon.Button name="share" size={23} backgroundColor="transparent" underlayColor="transparent" onPress={()=>{
+      <TouchableOpacity name="share" size={23} backgroundColor="transparent" underlayColor="transparent" onPress={()=>{
         const hotel = this.props.navigation.state.params.hotel;
         if (hotel && hotel._id) {
           Share.share({title:hotel.name, message:`${hotel.name}: ${hotel.description}`, url: hotel.website}, {subject:hotel.name});
         }
-      }}></Icon.Button>
+        
+      }}>
+      <Image source={require('../../assets/Share.jpg')}></Image>
+      </TouchableOpacity>
     );
   }
 };
